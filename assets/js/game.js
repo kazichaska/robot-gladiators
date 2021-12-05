7cebaf999b9afe22485b0693cfa081d6be8a01fd
Kazi Islam <kazichaska@gmail.com>
2021-12-05 15:42:56 -0600

    added console log stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+// variable for robot's name 
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+// window.alert(playerName);
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Out robot's name is " + playerName);
+
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
